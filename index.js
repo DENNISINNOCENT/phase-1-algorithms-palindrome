2d20ccd51bbb38b9cfde3f4699e4e135b7318d59
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-16 17:23:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //create a variable
+  //Take the string split it,reverse it and join it
+  //compare if the string provided is the same as returned
+  //use the condtional statements
+
+  let reverse =word.split("").reverse("").join('')
+  if(word ===reverse){
+    return true
+  }
+  else{
+    return false
+  }
 }
+  console.log(isPalindrome("a"))
+
+
 
 /* 
   Add your pseudocode here
 */
+//input===output ->true
+//input===!output ->false
 
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
+//You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
